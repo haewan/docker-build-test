14367d5a585ad57feeb57fda551083d878564245
haewan <haewan26@gmail.com>
2020-05-27 14:42:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python tag test!"
+    return "Hello from Python Haewan"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
